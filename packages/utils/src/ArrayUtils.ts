57c46bccc57d75b004dc9527796fecebc3865e6c
Michał Kędrzyński <kedrzu@gmail.com>
2023-05-25 12:28:32 +0200

    feat(utils): added arrayToMap

--- packages/utils/src/ArrayUtils.ts
+++ packages/utils/src/ArrayUtils.ts
@@ -105,3 +105,12 @@ export function findMax<T>(array: Iterable<T>, value: (item: T) => number) {
 
     return max;
 }
+
+export function arrayToMap<T, K>(array: Iterable<T>, key: (item: T) => K): Map<K, T> {
+    const map = new Map<K, T>();
+    for (const item of array) {
+        map.set(key(item), item);
+    }
+
+    return map;
+}
