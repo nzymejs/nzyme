c52558e3a3433793d765fe50adc76b659b742267
Michał Kędrzyński <kedrzu@gmail.com>
2023-07-05 18:47:57 +0200

    feat(dom): added scrollToTopElement helper

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -2,3 +2,4 @@ export * from './virtualHistory.js';
 export * from './requestIdleCallback.js';
 export * from './isBrowser.js';
 export * from './getOuterHeight.js';
+export * from './scrollToTopElement.js';
