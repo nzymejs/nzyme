bce78c5dbc03622c3d3d3f57d5e7fda409493545
Michał Kędrzyński <kedrzu@gmail.com>
2023-08-04 16:43:20 +0200

    fix(tests): fixed running unit tests

---
+++ packages/jest/src/typescript.ts
@@ -0,0 +1,24 @@
+import { Config } from '@jest/types';
+
+export function typescriptConfig(): Config.InitialOptions {
+    return {
+        preset: 'ts-jest/presets/default-esm', // or other ESM presets
+        moduleNameMapper: {
+            '^(\\.{1,2}/.*)\\.js$': '$1',
+        },
+        transform: {
+            '^.+\\.tsx?$': [
+                'ts-jest',
+                {
+                    useESM: true,
+                    tsconfig: '<rootDir>/tsconfig.tests.json',
+                },
+            ],
+        },
+        roots: ['<rootDir>'],
+        modulePaths: ['<rootDir>'],
+        moduleFileExtensions: ['ts', 'tsx', 'js'],
+        extensionsToTreatAsEsm: ['.ts'],
+        collectCoverageFrom: ['./dist/**/*.js'],
+    };
+}
--- packages/utils/.eslintrc.cjs
+++ packages/utils/.eslintrc.cjs
@@ -2,6 +2,13 @@ module.exports = {
     root: true,
     extends: [require.resolve('@nzyme/eslint/typescript')],
     parserOptions: {
-        project: `${__dirname}/tsconfig.json`,
+        project: [
+            `${__dirname}/tsconfig.json`,
+            // TS config for tests
+            `${__dirname}/tsconfig.tests.json`,
+        ],
+    },
+    rules: {
+        '@typescript-eslint/no-namespace': 'off',
     },
 };
