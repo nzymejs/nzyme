b3ecc6922f05e25cffecc83baa7b6bce0a9b7909
Michał Kędrzyński <kedrzu@gmail.com>
2023-12-01 13:18:12 +0100

    fix(vue): fixed Slots type

--- packages/vue/src/types/Slots.ts
+++ packages/vue/src/types/Slots.ts
@@ -1,6 +1,8 @@
+import type { VNode } from 'vue';
+
 import type { SomeObject } from '@nzyme/types';
 
 export type Slots<T> = {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [K in keyof T]: (props: undefined extends T[K] ? SomeObject : T[K]) => any;
+    [K in keyof T]: (props: undefined extends T[K] ? SomeObject : T[K]) => VNode[];
 };
