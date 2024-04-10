b4c4d671770e4ce67ad95c72770babd1e9dcb23e
Michał Kędrzyński <kedrzu@gmail.com>
2024-04-10 13:23:29 +0200

    feat(logging): extended Exception class

--- packages/logging/src/Exception.ts
+++ packages/logging/src/Exception.ts
@@ -1,9 +1,17 @@
+export type ExceptionOptions = {
+    cause?: unknown;
+    [key: string]: unknown;
+};
+
 export class Exception extends Error {
     constructor(
         message: string,
-        public readonly attrs?: Record<string, unknown>,
+        public readonly attrs?: ExceptionOptions,
     ) {
-        super(message);
+        super(message, {
+            cause: attrs?.cause,
+        });
+
         this.name = 'Exception';
     }
 }
