1e755e4d70b6f51ef518cca34a5d8863b98b5eb3
Michał Kędrzyński <kedrzu@gmail.com>
2024-04-29 11:26:30 +0200

    feat(ioc): added useServiceLazy

--- packages/vue/src/useContainer.ts
+++ packages/vue/src/useContainer.ts
@@ -1,11 +1,11 @@
-import { inject } from 'vue';
+import { inject, type InjectionKey } from 'vue';
 
 import type { Container } from '@nzyme/ioc';
 
-export const containerSymbol = Symbol('container');
+export const containerSymbol = Symbol('container') as InjectionKey<Container>;
 
 export function useContainer() {
-    const container = inject(containerSymbol) as Container;
+    const container = inject(containerSymbol);
     if (!container) {
         throw new Error('Container not registered. Register CommonPlugin first');
     }
--- packages/vue/src/useService.ts
+++ packages/vue/src/useService.ts
@@ -1,6 +1,7 @@
 import { effectScope } from 'vue';
 
 import type { Injectable } from '@nzyme/ioc';
+import { createMemo } from '@nzyme/utils';
 
 import { useContainer } from './useContainer.js';
 
@@ -9,4 +10,11 @@ const scope = effectScope(true);
 export function useService<T>(service: Injectable<T>): T {
     const container = useContainer();
     return scope.run(() => container.resolve(service)) as T;
+}
+
+export function useServiceLazy<T>(service: Injectable<T>): () => T {
+    const container = useContainer();
+    const memo = createMemo(() => scope.run(() => container.resolve(service)) as T);
+
+    return memo;
 }
