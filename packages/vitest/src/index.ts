e7fa5da2878ee5929d9d6315a0c50131075f719b
Michał Kędrzyński <kedrzu@gmail.com>
2024-10-16 11:56:01 +0200

    fix(vitest): fixed custom matcher typing

--- packages/vitest/src/index.ts
+++ packages/vitest/src/index.ts
@@ -1,16 +1,12 @@
-import type { expectToEqualFully } from './matchers/expectToEqualFully.js';
-import type { expectToEqualPartially } from './matchers/expectToEqualPartially.js';
+import 'vitest';
 
-type Matcher<F> = F extends (this: void, ...args: infer A) => unknown
-    ? (expected: A[1]) => void
-    : never;
-
-interface CustomMatchers<T> {
-    toEqualFully: Matcher<typeof expectToEqualFully<T>>;
-    toEqualPartially: Matcher<typeof expectToEqualPartially<T>>;
+interface CustomMatchers<T = unknown> {
+    toEqualFully: (expected: T) => void;
+    toEqualPartially: <E>(expected: E) => void;
 }
 
 declare module 'vitest' {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     interface Assertion<T = any> extends CustomMatchers<T> {}
+    interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
