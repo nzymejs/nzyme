f36fe52b86689b689c962f46a9643eadd296ed5b
Michał Kędrzyński <kedrzu@gmail.com>
2024-10-31 19:08:39 +0100

    fix(types): better type guards

--- packages/types/src/TypeGuards.ts
+++ packages/types/src/TypeGuards.ts
@@ -2,8 +2,8 @@ export type IfAny<T, Y, N = T> = 0 extends 1 & T ? Y : N;
 
 export type IfLiteral<T, Y, N = T> = T | 'jy@##O8sjzv=(+mby#4T=3gLHwU+0Z' extends T ? N : Y;
 
-export type IfNullable<T, Y, N> = IfAny<T, T, T | null extends T ? Y : N>;
+export type IfNullable<T, Y, N = T> = IfAny<T, T, T | null extends T ? Y : N>;
 
-export type IfUndefined<T, Y, N> = IfAny<T, T, T | undefined extends T ? Y : N>;
+export type IfUndefined<T, Y, N = T> = IfAny<T, T, T | undefined extends T ? Y : N>;
 
-export type IfUnknown<T, Y, N> = IfAny<T, T, unknown extends T ? Y : N>;
+export type IfUnknown<T, Y, N = T> = IfAny<T, T, unknown extends T ? Y : N>;
